8fd875e96e4e854db0ae2fbc5e7d35bb1001d650
malleshdevops <81004732+malleshdevops@users.noreply.github.com>
2021-07-30 07:49:49 +0530

    Update SpringBootRestAppApplication.java

--- src/main/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplication.java
+++ src/main/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootRestAppApplication {
 
+	String var=null;
+	String name=devops;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestAppApplication.class, args);
+		system.out.println("hello")
+			system.out.println("hello")
+			system.out.println("hello")
 	}
 
 }
